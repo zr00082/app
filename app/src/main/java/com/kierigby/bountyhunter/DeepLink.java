8c2d05fe51191b1991c7df205d3aebd7582be2d5
ZAID RAHHAL <zr00082@surrey.ac.uk>
2019-03-25 14:48:42 +0000

    Register view done

--- app/src/main/java/com/kierigby/bountyhunter/DeepLink.java
+++ app/src/main/java/com/kierigby/bountyhunter/DeepLink.java
@@ -3,7 +3,7 @@ package com.kierigby.bountyhunter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Main2Activity extends AppCompatActivity {
+public class DeepLink extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
