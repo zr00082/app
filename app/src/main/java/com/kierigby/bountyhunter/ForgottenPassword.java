165d849e906efbca9dcecb74f12ec8a7b9ea5a29
Azwad Rashid <azu.rashid1998@gmail.com>
2019-04-02 23:25:35 +0100

    Completed New Password Activity

--- app/src/main/java/com/kierigby/bountyhunter/ForgottenPassword.java
+++ app/src/main/java/com/kierigby/bountyhunter/ForgottenPassword.java
@@ -5,9 +5,10 @@ import android.os.Bundle;
 
 public class ForgottenPassword extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_forgotten_password);
+        setContentView(R.layout.activity_forgotten_pass);
     }
 }
--- app/src/main/java/com/kierigby/bountyhunter/NewPasswordActivity.java
+++ app/src/main/java/com/kierigby/bountyhunter/NewPasswordActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.bountyhunterapi.BountyHunterAPI;
@@ -16,41 +17,58 @@ import com.example.bountyhunterapi.Token;
 public class NewPasswordActivity extends AppCompatActivity {
     private EditText mNewPasswordInput,mConfirmNewPasswordInput;
     private Button mNewPasswordBtn;
+    private ImageButton mBackArrow;
     private BountyHunterAPI api = new BountyHunterAPI(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_password);
-//        addListenerToNewPasswordButton();
+        addListenerToNewPasswordButton();
+        addListenerToBackButton();
+
     }
 
-//    public void addListenerToNewPasswordButton(){
-//        mNewPasswordInput=findViewById();
-//        mConfirmNewPasswordInput=findViewById();
-//        mNewPasswordBtn=findViewById();
-//
-//        mNewPasswordBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if(mNewPasswordInput.getText().toString().isEmpty()||mConfirmNewPasswordInput.getText().toString().isEmpty()){
-//                    Toast.makeText(getApplicationContext(), "Please enter all the necessary information", Toast.LENGTH_LONG).show();
-//                } else if (!mNewPasswordInput.getText().toString().equals(mConfirmNewPasswordInput.getText().toString())) {
-//                    Toast.makeText(getApplicationContext(), "The passwords you entered do not match", Toast.LENGTH_LONG).show();
-//                }else{
-//                    api.resetPassoword(getToken(),mConfirmNewPasswordInput.getText().toString());
-//                                    NavUtils.navigateUpFromSameTask(NewPasswordActivity.this);
-//                }
-//            }
-//        });
-//    }
+    public void addListenerToNewPasswordButton(){
+        mNewPasswordInput=findViewById(R.id.etNewPassword);
+        mConfirmNewPasswordInput=findViewById(R.id.etConfirmNewPassword);
+        mNewPasswordBtn=findViewById(R.id.btSubmit);
 
-    public String getToken(){
-        Intent deepLinkI= getIntent();
-        String token= "Bearer " +deepLinkI.getData().getQueryParameter("token");
-        Toast.makeText(getApplicationContext(),token,Toast.LENGTH_LONG).show();
+        mNewPasswordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mNewPasswordInput.getText().toString().isEmpty()||mConfirmNewPasswordInput.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Please enter all the necessary information", Toast.LENGTH_LONG).show();
+                } else if (!mNewPasswordInput.getText().toString().equals(mConfirmNewPasswordInput.getText().toString())) {
+                    Toast.makeText(getApplicationContext(), "The passwords you entered do not match", Toast.LENGTH_LONG).show();
+                }else{
+                    api.resetPassoword(getToken(),mConfirmNewPasswordInput.getText().toString());
+                                    NavUtils.navigateUpFromSameTask(NewPasswordActivity.this);
+                }
+            }
+        });
 
-        return token;
+
     }
 
+    public void addListenerToBackButton() {
+        mBackArrow = findViewById(R.id.backFromSetNewPass);
+        mBackArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent backToMainPage = new Intent(NewPasswordActivity.this, MainActivity.class);
+                startActivity(backToMainPage);
+
+
+            }
+        });
+    }
+            public String getToken() {
+                Intent deepLinkI = getIntent();
+                String token = "Bearer " + deepLinkI.getData().getQueryParameter("token");
+                Toast.makeText(getApplicationContext(), token, Toast.LENGTH_LONG).show();
+
+                return token;
+
+        }
 }
