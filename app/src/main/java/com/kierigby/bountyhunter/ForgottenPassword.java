582949b49b5b6e52e41e8df63ef07f0e3bbbbc52
ZAID RAHHAL <zr00082@surrey.ac.uk>
2019-04-02 17:52:24 +0300

    Added delete user

---
+++ app/src/main/java/com/kierigby/bountyhunter/ForgottenPassword.java
@@ -0,0 +1,13 @@
+package com.kierigby.bountyhunter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ForgottenPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgotten_password);
+    }
+}
