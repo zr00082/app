eba6517cac0da6d72905e6ceb2cbad32d0979953
ZAID RAHHAL <zr00082@surrey.ac.uk>
2019-05-10 00:09:43 +0100

    bounty hunter activity and view notification

---
+++ app/src/main/java/com/kierigby/bountyhunter/BountyHunterActivity.java
@@ -0,0 +1,87 @@
+package com.kierigby.bountyhunter;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.View;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class BountyHunterActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private DrawerLayout drawer;
+    private SupportMapFragment mapFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bounty_hunter);
+
+        drawer = findViewById(R.id.drawer);
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) mapFragment.getMapAsync(this);
+
+        findViewById(R.id.ivHam).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (drawer.isDrawerOpen(Gravity.END)) {
+                    drawer.closeDrawer(Gravity.END);
+                } else {
+                    drawer.openDrawer(Gravity.END);
+                }
+            }
+        });
+
+        findViewById(R.id.ivCross).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (drawer.isDrawerOpen(Gravity.END)) {
+                    drawer.closeDrawer(Gravity.END);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng sydney = new LatLng(30.733315, 76.779419);
+
+        try {
+            ConstraintLayout layout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.layout_custom_marker, null);
+
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+            layout.setLayoutParams(new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT,
+                    ConstraintLayout.LayoutParams.WRAP_CONTENT));
+            layout.measure(displayMetrics.widthPixels, displayMetrics.heightPixels);
+            layout.layout(0, 0, displayMetrics.widthPixels, displayMetrics.heightPixels);
+            layout.buildDrawingCache();
+            Bitmap bitmap = Bitmap.createBitmap(layout.getMeasuredWidth(), layout.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+
+            Canvas canvas = new Canvas(bitmap);
+            layout.draw(canvas);
+
+            googleMap.addMarker(new MarkerOptions()
+                    .position(sydney)
+                    .icon(BitmapDescriptorFactory.fromBitmap(bitmap)));
+
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
